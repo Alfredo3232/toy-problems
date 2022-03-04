5c183e601e764e6d376e336f29080dd1f54f75bd
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 17:14:04 -0600

    Update oddOrEven.js

--- week0/oddOrEven.js
+++ week0/oddOrEven.js
@@ -1,4 +1,4 @@
-// Using conditional statements, your logic should print the given number is "even" or "odd"
+// Using conditional statements, your logic should print the given number "even" or "odd"
 var number = 3;
 if ( number % 2 === 0){
   console.log("even");
