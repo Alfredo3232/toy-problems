d4a141aef7b390f3b7c465752022d174c98ac6df
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 17:11:02 -0600

    Update simpleMath.js

--- week0/simpleMath.js
+++ week0/simpleMath.js
@@ -1,3 +1,3 @@
-//print to the screen a simple math expresssion that will resutl in the number 37
+//print to the screen a simple math expresssion that will result in the number 37
 
 console.log( 9 * 2 + 13 - 50 + 56 )
