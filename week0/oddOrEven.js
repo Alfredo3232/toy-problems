ce0ef9275ff5c3a36841bce4dd73d777f820d84c
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 14:11:32 -0600

    Create oddOrEven.js

---
+++ week0/oddOrEven.js
@@ -0,0 +1,7 @@
+// write a if and else statements that prints if the given number is even or odd
+var number = 3;
+if ( number % 2 === 0){
+  console.log("even");
+} else {
+  console.log( "odd");
+}
