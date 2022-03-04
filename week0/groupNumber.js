213ff65a9ce5dc5809a1e88c9e04ad0dff625a57
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 17:07:10 -0600

    Update groupNumber.js

--- week0/groupNumber.js
+++ week0/groupNumber.js
@@ -1,9 +1,10 @@
+//create logic when given a interger in peopleNumber, 0 or lower should print "No People", 1 "a solo", 2 "a duo, 3 "a trio", 4 "a squad"
 var peopleNumber = 4;
-//use if else statements to properly name a group of people according to the number of the variable peopleNumber
+
 if (peopleNumber <= 0){
   console.log( "No People");
 } else if ( peopleNumber === 1){
-  console.log("One Person");
+  console.log("Solo");
 }else if ( peopleNumber === 2){
   console.log("A Duo");
 }else if ( peopleNumber === 3){
