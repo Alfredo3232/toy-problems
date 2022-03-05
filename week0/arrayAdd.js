c502854a78e41df53421a020917c32612f1ba868
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 21:43:41 -0600

    Create arrayAdd.js

---
+++ week0/arrayAdd.js
@@ -0,0 +1,8 @@
+// create an array and add 3 more elements to the array
+
+var ramen = ["shrimp", "seafood", "beef", "tonkotsu", "chicken"];
+
+ramen.push("pork");
+ramen.push("soy sauce");
+ramen.push("creamy chicken");
+console.log(ramen)
