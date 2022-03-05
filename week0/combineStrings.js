b633ea0f1c0d227086c84c276f530232c876a6b8
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-05 08:08:54 -0600

    Update combineStrings.js

--- week0/combineStrings.js
+++ week0/combineStrings.js
@@ -1,4 +1,4 @@
-//Assign two variables string values and use concentation two print them in one console.log
+//Assign two variables with string values and use concentation two print them in one console.log
 var one = "Hello";
 var two = "World!";
 console.log( one + " " + two)
