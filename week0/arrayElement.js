300e259a0da4e3e4758cb0132bda07265645ada7
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-05 08:04:48 -0600

    Update arrayElement.js

--- week0/arrayElement.js
+++ week0/arrayElement.js
@@ -1,4 +1,4 @@
-// create an array with at least 5 elements and change 3 out of the 5 elements of your array into something else
+// create an array with at least 5 string elements and change 3 out of the 5 elements of your array into something else
  
 var ramen = ["shrimp", "seafood", "beef", "tonkotsu", "chicken"];
 
