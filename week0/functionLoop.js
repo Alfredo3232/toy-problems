e426ffa1bc4e28b3e8b6440298398098515e82d7
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-05 08:34:56 -0600

    Update functionLoop.js

--- week0/functionLoop.js
+++ week0/functionLoop.js
@@ -1,4 +1,4 @@
-// write a function a function when given a interger it returns that number of "yes"
+// write a function when given a interger it returns that number of "yes"
  var result = "";
 function agree(num) {
    for( var i = 0; i < num; i++){
