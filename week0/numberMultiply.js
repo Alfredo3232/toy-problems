90467acac00363a930f3cf518555fa3817ce3294
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 18:47:23 -0600

    Create numberMultiply.js

---
+++ week0/numberMultiply.js
@@ -0,0 +1,7 @@
+// create a for loop when given a interger in variable num, it multiplies all of the factorials of that number
+var solution = 1;
+
+for (var num = 12; num >= 1; num = num - 1){
+        solution = solution * num
+}
+console.log(solution);
