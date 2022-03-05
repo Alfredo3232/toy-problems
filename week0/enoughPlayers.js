f3a002150964a7fba015fc61d4b16e50548aee83
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 21:40:22 -0600

    Create enoughPlayers.js

---
+++ week0/enoughPlayers.js
@@ -0,0 +1,12 @@
+/* You are trying to build a esports team, but you need at least 5 players in your team.
+create a function that checks the amount of players in your array, returns true if you have 5 players and false if you don't
+ */
+var team = ["Me", "Friend 1", "Friend 2", "Friend 3", "Friend 4"];
+function hasEnoughPlayers(){
+  if (team.length >= 5){
+      return true;
+  } else{
+      return false;
+  }
+}
+console.log(hasEnoughPlayers(team));
