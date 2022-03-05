1fde8552c4bb8629c363de4a97cfe08e723c9fa8
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 21:29:31 -0600

    Create arrayElement.js

---
+++ week0/arrayElement.js
@@ -0,0 +1,8 @@
+// create an array with at least 5 elements and change 3 out of the 5 elements of your array into something else
+ 
+var ramen = ["shrimp", "seafood", "beef", "tonkotsu", "chicken"];
+
+ramen[1] = "pork";
+ramen[3] = "soy sauce";
+ramen[4] = "creamy chicken";
+console.log(ramen)
