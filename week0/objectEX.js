1603bf7af6688e144ecae025ea1b8350cad0b6c2
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-04 21:59:10 -0600

    Create objectEX.js

---
+++ week0/objectEX.js
@@ -0,0 +1,7 @@
+//create an object with the name person and create a random person with the values "name", "age", an array named hobbies and then print your object
+var person = {
+  name: "John Doe",
+  age: 28,
+  hobbies:  [ "exercise", "Reading", "Sports"]
+}
+console.log(person)
