21eee436204d35e4359b08c2bf3c16985e2012a1
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-05 08:32:09 -0600

    Update enoughPlayers.js

--- week0/enoughPlayers.js
+++ week0/enoughPlayers.js
@@ -2,7 +2,7 @@
 create a function that checks the amount of players in your array, returns true if you have 5 players and false if you don't
  */
 var team = ["Me", "Friend 1", "Friend 2", "Friend 3", "Friend 4"];
-function hasEnoughPlayers(){
+function hasEnoughPlayers(team){
   if (team.length >= 5){
       return true;
   } else{
