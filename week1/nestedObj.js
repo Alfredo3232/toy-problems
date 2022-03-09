eb81555975aa4410b46baadc51252961fb1aa9a6
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-08 20:45:24 -0600

    Create nestedObj.js

---
+++ week1/nestedObj.js
@@ -0,0 +1,8 @@
+//create an object inside another object with at least 1 key:value in both objects then access that nested object's key:value
+let object1 = {
+  str: "Hey",
+  object2: {
+    num: 3
+  }
+}
+object1.object2
