870dab942b0180b4844c43fea5d833b17ea49cc8
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-21 17:27:17 -0600

    Create edadParaBeber.js

---
+++ edadParaBeber.js
@@ -0,0 +1,10 @@
+/*Escriba una función llamada "edadParaBeber". Dado un número, en este caso una edad, "edadParaBeberk" devuelve si una persona tiene la edad suficiente para beber 
+legalmente en Mexico. 
+Notas:
+La edad para beber en Mexico es 18.
+*/
+function edadParaBeber(edad) {
+    //tu codigo aqui
+}
+var final = edadParaBeber(16);
+console.log(final);
