55a408e2979cbbad49a26bde237ab666d76aa063
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-23 21:45:18 -0600

    Create functionReturnIF.js

---
+++ 0intro/functionReturnIF.js
@@ -0,0 +1,11 @@
+//write a function when given a number, it return true if the number is greater than 11
+function biggerThan11(num) {
+  if( num > 11){
+    return true;
+  } else{
+    return "not greater than eleven :(";
+  }
+}
+
+var output = biggerThan11(11);
+console.log(output); 
