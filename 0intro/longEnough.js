b859e0955954576c54cb1b8af88b7766dae79c8d
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-03-24 22:00:50 -0600

    Create longEnough.js

---
+++ 0intro/longEnough.js
@@ -0,0 +1,12 @@
+/* write a function, when given a username and a password returns true if username is longer than 4 characters and the password is at least 8 characters long, and false 
+if false
+*/
+ function valid(username, password) {
+    if(name.length > 4 &&   password.length  >= 8){
+      return true;
+    } else{
+      return false;
+    }
+}
+var output = valid('ExampleUsername', 'ExamplePassword')
+console.log(output);
