6241c0e095e2c041341a0f9759711465e502c7ef
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-06-27 21:24:37 -0700

    Create obtieneLaMediaDeUnaMatriz.js

---
+++ 9/obtieneLaMediaDeUnaMatriz.js
@@ -0,0 +1,12 @@
+/* 
+Es el final del año académico, momento fatídico de tu informe escolar. Los promedios deben ser calculados. Todos los estudiantes vienen a ti y te suplican que les calcules el promedio.
+
+Devuelve el promedio de la matriz dada redondeado hacia abajo a su entero más cercano.
+
+La matriz nunca estará vacía.
+*/
+
+
+function obtieneElPromedio(grados){
+
+}
