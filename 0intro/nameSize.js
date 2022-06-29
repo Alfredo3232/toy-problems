d02bafc4d6e4cc9db83546e1939cc000390baa65
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-06-29 10:17:29 -0700

    just renamed a folder

--- 0intro/nameSize.js
+++
@@ -1,6 +0,0 @@
-//create logic when a string is given in nameSize(name), return a interger according the amount of characters in that string
-var nameSize = function (name) {
-  return name.length
-}
-
-nameSize("alfred")
---
+++ intro/nameSize.js
@@ -0,0 +1,6 @@
+//create logic when a string is given in nameSize(name), return a interger according the amount of characters in that string
+var nameSize = function (name) {
+  return name.length
+}
+
+nameSize("alfred")
