b67030f28b2378fec52585e53e3a3ec28db35d19
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-02-16 16:43:43 -0600

    add comment in eslintrc

--- currentProblem.js
+++ currentProblem.js
@@ -1,7 +1,22 @@
-function invert(array) {
-    let newArr = [];
-    for (let i = 0; i < array.length; i++) {
-        newArr.push(array[i] * -1);
+function findOutlier(int) {
+    let int2 = [...int];
+
+    let even = 0;
+    let odd = 0;
+
+    for (let i = 0; i < int2.length; i++) {
+        if (int2[i] < 0) int2[i] = Math.abs(int2[i]);
+
+        int2[i] % 2 === 0 ? odd++ : even++;
     }
-    return newArr;
+
+    if (odd > even) {
+        for (let i = 0; i < int2.length; i++) {
+            if (int2[i] % 2) return int[i];
+        }
+    } else {
+        for (let i = 0; i < int2.length; i++) {
+            if (!(int2[i] % 2)) return int[i];
+        }
+    }
 }
