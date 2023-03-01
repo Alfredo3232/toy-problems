573e6a45fdbbe6c5479a607ebfd8870b20eab0d1
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-03-01 13:15:39 -0600

    update deps and solve toy problem

--- Problems/intro/addOdds.js
+++ Problems/intro/addOdds.js
@@ -2,6 +2,7 @@
 make a function that takes in an array of values, only add up the odd numbers and filter out the non numbers in the given input array=
 addOdds(["hello", true, 1, 5, false, 20, 21];);
 */
+
 function addOdds(numArray) {
   let result = 0;
   for (let i = 0; i < numArray.length; i++) {
--- currentProblem.js
+++ currentProblem.js
@@ -1,22 +1,9 @@
-function findOutlier(int) {
-    let int2 = [...int];
+function reverseWords(str) {
+    let arr = str.split(" ");
 
-    let even = 0;
-    let odd = 0;
-
-    for (let i = 0; i < int2.length; i++) {
-        if (int2[i] < 0) int2[i] = Math.abs(int2[i]);
-
-        int2[i] % 2 === 0 ? odd++ : even++;
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].split("").reverse().join("");
     }
 
-    if (odd > even) {
-        for (let i = 0; i < int2.length; i++) {
-            if (int2[i] % 2) return int[i];
-        }
-    } else {
-        for (let i = 0; i < int2.length; i++) {
-            if (!(int2[i] % 2)) return int[i];
-        }
-    }
+    return arr.join(" ");
 }
