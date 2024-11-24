38aa6edc758bc58f6a69bc8fc8ef29ec8cbae9d2
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2024-11-23 21:40:34 -0800

    Update currentProblem

--- currentProblem.js
+++ currentProblem.js
@@ -1,10 +1,14 @@
-let updateLight = (current) => {
-    switch (current) {
-        case "green":
-            return "yellow";
-        case "yellow":
-            return "red";
-        case "red":
-            return "green";
+let count = (str) => {
+    if (str.length < 1) return {};
+
+    let count = {};
+    for (let i = 0; i < str.length; i++) {
+        count[`${str[i]}`] = 0;
+
+        for (let j = 0; j < str.length; j++) {
+            if (str[i] === str[j]) count[`${str[i]}`] += 1;
+        }
     }
+
+    return count;
 };
